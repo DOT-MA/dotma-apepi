631094aae7f7bf2d6f5b615c463b54197affa7a6
thinc5 <thinc360@gmail.com>
2019-01-20 15:07:07 +1000

    init

---
+++ src/ContentRetrival.ts
@@ -0,0 +1,10 @@
+export class ContentRetrival {
+  // Requests a random sound from the DOTMA resources page
+  public static requestSound():boolean {
+    // Check function is called inside api request
+    console.log("Yeet");
+    return true;
+  };
+
+};
+
---
+++ test/test.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todos = [
+    {
+        id: 1,
+        title: "lunch",
+        description: "Go for lunc by 2pm"
+    }
+];
+exports.default = todos;
