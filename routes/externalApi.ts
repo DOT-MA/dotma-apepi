314d88e688ab97406c7268647359e63a3daaed7e
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-25 18:20:44 +1000

    Move server components to new repo

--- routes/externalApi.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express";
-
-export const router = express.Router();
-
-router.get("/testExternalApi", (req, res) => {
-    const url = "https://raikou1.donmai.us/55/2e/552e74406af8a2b6b8b7fd547bc2c353.jpg";
-    res.status(200).send(`<img src=${url}/>"`);
-});
--- routes/index.ts
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-
-export const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.send("Welcome to DotmaAPI.");
-});
